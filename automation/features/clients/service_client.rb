269cb5b8b9b3aea5ed83de73b11bc355259c5d90
mpenna <tworker@onyx.ove.com>
2017-03-28 08:50:18 -0400

    added auth methods to service client

--- automation/features/clients/service_client.rb
+++ automation/features/clients/service_client.rb
@@ -10,8 +10,16 @@ class ServiceClient
       return HTTParty.post((get_base_uri + path).to_str, :body=> body, :headers => { 'Content-Type' => 'application/json' })
     end
 
+    def post_to_url_with_auth(path, body, authHeader)
+      return HTTParty.post((get_base_uri + path).to_str, :body=> body, :headers => { 'Content-Type' => 'application/json', 'Authorization' => authHeader })
+    end
+
     def get_by_url(url)
       return HTTParty.get(url);
+    end
+
+    def get_by_url_with_auth(url, authHeader)
+      return HTTParty.get(url, :headers => { 'Authorization' => authHeader });
     end
 
   end
