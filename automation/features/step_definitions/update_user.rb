0803036c4a1f46bbc70944197b4340d4d3f51220
mpenna <tworker@onyx.ove.com>
2017-05-09 13:55:45 -0400

    Fixed update user automation tests for authentication

--- automation/features/step_definitions/update_user.rb
+++ automation/features/step_definitions/update_user.rb
@@ -1,69 +1,80 @@
 require 'date'
 
 When(/^A request to update a user that does not exists$/) do
+  basic_auth = Base64.encode64( "ofssystemadmin:p@$$Wordofs")
+  authToken = @service_client.get_by_url_with_auth(@service_client.get_base_uri.to_s+"/users/getToken", "Basic "+ basic_auth)['token']
   name = Faker::Pokemon.name + (SecureRandom.random_number(999) + 1000).to_s
   email = name + "@pokemon.com"
   user = FactoryGirl.build(:user, userName: name, emailAddress: email)
-  @result = @service_client.post_to_url("/users/id/123", user.update_to_json)
+  @result = @service_client.post_to_url_with_auth("/users/id/123", user.update_to_json, "Bearer "+ authToken)
 end
 
 When(/^A request to update the user with invalid (.*?)$/) do |field|
+  authToken = @user.token
   @user = FactoryGirl.build(:user,  company_href: @company.href, company_name: @company.name)
   body = @user.update_to_hash
   body[field]="test"
-  @result = @service_client.post_to_url("/users/id/"+ @location.split("/id/").last, body.to_json)
+  @result = @service_client.post_to_url_with_auth("/users/id/"+ @location.split("/id/").last, body.to_json, "Bearer " + authToken)
 end
 
 When(/^A request to update the user tokenExpDate$/) do
+  authToken = @user.token
   @user = FactoryGirl.build(:user,  company_href: @company.href, company_name: @company.name)
   body = @user.update_to_hash
   @updatedValue = DateTime.now.strftime "%Y-%m-%dT%H:%M:%SZ"
   body["tokenExpDate"]=@updatedValue
-  @result = @service_client.post_to_url("/users/id/"+ @location.split("/id/").last, body.to_json)
+  @result = @service_client.post_to_url_with_auth("/users/id/"+ @location.split("/id/").last, body.to_json, "Bearer " + authToken)
 end
 
 And(/^I should see the (.*?) was updated$/) do |field|
-  @result = @service_client.get_by_url(@location)
+  basic_auth = Base64.encode64( "ofssystemadmin:p@$$Wordofs")
+  authToken = @service_client.get_by_url_with_auth(@service_client.get_base_uri.to_s+"/users/getToken", "Basic "+ basic_auth)['token']
+  @result = @service_client.get_by_url_with_auth(@location, "Bearer " + authToken)
   expect(@result[field]).to eql @updatedValue
 end
 
 When(/^A request to update the user activeFlag$/) do
+  authToken = @user.token
   @user = FactoryGirl.build(:user,  company_href: @company.href, company_name: @company.name)
   body = @user.update_to_hash
   @updatedValue = false
   body["activeFlag"]=@updatedValue
-  @result = @service_client.post_to_url("/users/id/"+ @location.split("/id/").last, body.to_json)
+  @result = @service_client.post_to_url_with_auth("/users/id/"+ @location.split("/id/").last, body.to_json, "Bearer " + authToken)
 end
 
 When(/^A request to update the user firstName$/) do
+  authToken = @user.token
   @user = FactoryGirl.build(:user,  company_href: @company.href, company_name: @company.name)
   body = @user.update_to_hash
   @updatedValue = "test"
   body["firstName"]=@updatedValue
-  @result = @service_client.post_to_url("/users/id/"+ @location.split("/id/").last, body.to_json)
+  @result = @service_client.post_to_url_with_auth("/users/id/"+ @location.split("/id/").last, body.to_json, "Bearer " + authToken)
 end
 
 When(/^A request to update the user lastName$/) do
+  authToken = @user.token
   @user = FactoryGirl.build(:user,  company_href: @company.href, company_name: @company.name)
   body = @user.update_to_hash
   @updatedValue = "test"
   body["lastName"]=@updatedValue
-  @result = @service_client.post_to_url("/users/id/"+ @location.split("/id/").last, body.to_json)
+  @result = @service_client.post_to_url_with_auth("/users/id/"+ @location.split("/id/").last, body.to_json, "Bearer " + authToken)
 end
 
 When(/^A request to update the user role$/) do
+  authToken = @user.token
   @user = FactoryGirl.build(:user,  company_href: @company.href, company_name: @company.name)
   body = @user.update_to_hash
   @updatedValue = "ADMIN"
   body["role"]=@updatedValue
-  @result = @service_client.post_to_url("/users/id/"+ @location.split("/id/").last, body.to_json)
+  @result = @service_client.post_to_url_with_auth("/users/id/"+ @location.split("/id/").last, body.to_json, "Bearer " + authToken)
 end
 
 When(/^A request to update the user role with invalid role$/) do
+  authToken = @user.token
   @user = FactoryGirl.build(:user,  company_href: @company.href, company_name: @company.name)
   body = @user.update_to_hash
   body["role"]="TEST"
-  @result = @service_client.post_to_url("/users/id/"+ @location.split("/id/").last, body.to_json)
+  @result = @service_client.post_to_url_with_auth("/users/id/"+ @location.split("/id/").last, body.to_json, "Bearer " + authToken)
 end
 
 And(/^I should see an error message indicating role invaild enum$/) do
@@ -71,19 +82,21 @@ And(/^I should see an error message indicating role invaild enum$/) do
 end
 
 When(/^A request to update the user password$/) do
+  authToken = @user.token
   @user = FactoryGirl.build(:user,  company_href: @company.href, company_name: @company.name)
   body = @user.update_to_hash
   @updatedValue = "test"
   body["password$"]=@updatedValue
-  @result = @service_client.post_to_url("/users/id/"+ @location.split("/id/").last, body.to_json)
+  @result = @service_client.post_to_url_with_auth("/users/id/"+ @location.split("/id/").last, body.to_json, "Bearer " + authToken)
 end
 
 When(/^A request to update the user emailAddress$/) do
+  authToken = @user.token
   @user = FactoryGirl.build(:user,  company_href: @company.href, company_name: @company.name)
   body = @user.update_to_hash
   @updatedValue = "test@test.com"
   body["emailAddress"]=@updatedValue
-  @result = @service_client.post_to_url("/users/id/"+ @location.split("/id/").last, body.to_json)
+  @result = @service_client.post_to_url_with_auth("/users/id/"+ @location.split("/id/").last, body.to_json, "Bearer " + authToken)
 end
 
 Then(/^I should see the password has changed$/) do
--- automation/features/update_user.feature
+++ automation/features/update_user.feature
@@ -6,85 +6,85 @@ Feature: User is updated when update user endpoint is called
     Then the response should have a status of 404
 
   Scenario: Try to update a users id
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user with invalid id
     Then the response should have a status of 400
     And I should see an error message with id not allowed
 
   Scenario: Try to update a users href
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user with invalid href
     Then the response should have a status of 400
     And I should see an error message with href not allowed
 
   Scenario: Try to update a users createdOn
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user with invalid createdOn
     Then the response should have a status of 400
     And I should see an error message with createdOn not allowed
 
   Scenario: Try to update a users token
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user with invalid token
     Then the response should have a status of 400
     And I should see an error message with token not allowed
 
   Scenario: Try to update a users company
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user with invalid company
     Then the response should have a status of 400
     And I should see an error message with company not allowed
 
   Scenario: Try to update a users userName
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user with invalid userName
     Then the response should have a status of 400
     And I should see an error message with userName not allowed
 
   Scenario: Try to update a users tokenExpDate
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user tokenExpDate
     Then the response should have a status of 204
     And I should see the tokenExpDate was updated
 
   Scenario: Try to update a users activeFlag
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user activeFlag
     Then the response should have a status of 204
     And I should see the activeFlag was updated
 
   Scenario: Try to update a users firstName
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user firstName
     Then the response should have a status of 204
     And I should see the firstName was updated
 
   Scenario: Try to update a users lastName
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user lastName
     Then the response should have a status of 204
     And I should see the lastName was updated
 
   Scenario: Try to update a users role
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user role
     Then the response should have a status of 204
     And I should see the role was updated
 
   Scenario: Try to update a users role
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user role with invalid role
     Then the response should have a status of 400
     And I should see an error message indicating role invaild enum
 
   Scenario: Try to update a users password
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user password
     Then the response should have a status of 204
     And I should see the password has changed
 
   Scenario: Try to update a users emailAddress
-    Given A company and user exists
+    Given A company and user exists with token
     When A request to update the user emailAddress
     Then the response should have a status of 204
     And I should see the emailAddress was updated
